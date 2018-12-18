bfc44113e4af21b449f443799983873887e0759a
clessiomendes <clessiomendes@yahoo.com>
2018-12-18 02:32:35 -0200

    novo ambiente: validacao

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -5,9 +5,22 @@ grails.gorm.default.constraints = {
 grails.views.gsp.encoding = "UTF-8"
 grails.converters.encoding = "UTF-8"
 
+String aHost = System.getenv('POSTGRESQL_ADDON_HOST') ?: "localhost";
+String aPort = System.getenv('POSTGRESQL_ADDON_HOST') ?: "5432";
+String aDb = System.getenv('POSTGRESQL_ADDON_HOST') ?: "sa";
+String aUsername = System.getenv('POSTGRESQL_ADDON_HOST') ?: "postgres";
+String aPassword = System.getenv('POSTGRESQL_ADDON_HOST') ?: "senha";
+
+//Configuracoes gerais para todos os ambientes (sobrescrever configuracoes de ambientes especificos na sequencia)
 dataSource {
     dialect = "org.hibernate.dialect.PostgreSQLDialect"
     driverClassName = "org.postgresql.Driver"
+
+    url = "jdbc:postgresql://${aHost}:${aPort}/${aDb}"
+    username = aUsername
+    password = aPassword
+
+    dbCreate = "update"
 
     minEvictableIdleTimeMillis = 1800000
     timeBetweenEvictionRunsMillis = 1800000
@@ -27,26 +40,23 @@ dataSource {
 environments {
     development {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:postgresql://localhost:5432/sa"
-            username = "postgres"
-            password = "senha"
+//            dbCreate = "update"
+//            url = "jdbc:postgresql://localhost:5432/sa"
+//            username = "postgres"
+//            password = "senha"
         }
     }
     validacao {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:postgresql://${System.getenv('POSTGRESQL_ADDON_HOST')}:${System.getenv('POSTGRESQL_ADDON_PORT')}/${System.getenv('POSTGRESQL_ADDON_DB')}"
-            username = System.getenv('POSTGRESQL_ADDON_HOST')
-            password = System.getenv('POSTGRESQL_ADDON_HOST')
+//            dbCreate = "update"
+//            url = "jdbc:postgresql://${}:${System.getenv('POSTGRESQL_ADDON_PORT')}/${System.getenv('POSTGRESQL_ADDON_DB')}"
+//            username = System.getenv('POSTGRESQL_ADDON_USER')
+//            password = System.getenv('POSTGRESQL_ADDON_PASSWORD')
         }
     }
     production {
         dataSource {
             dbCreate = "validate"
-            url = "jdbc:postgresql://${System.getenv('POSTGRESQL_ADDON_HOST')}:${System.getenv('POSTGRESQL_ADDON_PORT')}/${System.getenv('POSTGRESQL_ADDON_DB')}"
-            username = System.getenv('POSTGRESQL_ADDON_HOST')
-            password = System.getenv('POSTGRESQL_ADDON_HOST')
         }
     }
 }
