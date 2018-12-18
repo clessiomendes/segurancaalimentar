7bfea7e2bbe56960a7fd82c9a82e504613137eb7
clessiomendes <clessiomendes@yahoo.com>
2018-12-17 22:22:38 -0200

    no message

--- grails-app/init/segurancaalimentar/Application.groovy
+++ grails-app/init/segurancaalimentar/Application.groovy
@@ -5,8 +5,11 @@ import grails.boot.config.GrailsAutoConfiguration
 
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
-        System.out.println("Application main System.properties['URL_DB'] "+System.properties["URL_DB"]);
-        System.out.println("Application main System.getProperty('URL_DB') "+System.getProperty("URL_DB"));
+        System.out.println("Application main System.properties['APP_HOME'] "+System.getProperty("APP_HOME"));
+        System.out.println("Application main System.getenv('APP_HOME'] "+System.getenv("APP_HOME"));
+        System.out.println("Application main System.getProperty['APP_HOME'] "+System.properties["APP_HOME"]);
+//        System.out.println("Application main System.getProperty('JAVA_VERSION') "+System.getProperty("JAVA_VERSION"));
+//        System.out.println("Application main System.getProperty('JAVA_VERSION') "+System.properties["JAVA_VERSION"]);
         GrailsApp.run(Application, args)
     }
 }
